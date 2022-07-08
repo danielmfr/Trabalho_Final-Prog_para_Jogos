dcab5184bd2324ec7eb41db3f2094baaf0c4e242
Ribas5 <51717679+Ribas5@users.noreply.github.com>
2022-07-08 15:27:56 -0300

    ajuste morte do jogador

--- Scenes/characters/Player.gd
+++ Scenes/characters/Player.gd
@@ -4,6 +4,7 @@ var movespeed = 400
 var bullet = preload("res://Scenes/props/Bullet.tscn")
 
 var recarregado = true
+var morto = false
 
 func _ready():
 	Global.jogador = self
@@ -13,22 +14,22 @@ func _enter_tree():
 
 func _physics_process(delta):
 	var motion = Vector2()
+	if !morto:
+		if Input.is_action_pressed("up"):
+			motion.y -= 1
+		if Input.is_action_pressed("down"):
+			motion.y += 1
+		if Input.is_action_pressed("left"):
+			get_node( "Sprite" ).set_flip_h( true )
+			motion.x -= 1
+		if Input.is_action_pressed("right"):
+			get_node( "Sprite" ).set_flip_h( false )
+			motion.x += 1 
+		
+		motion = motion.normalized()
+		motion = move_and_slide(motion * movespeed)
 	
-	if Input.is_action_pressed("up"):
-		motion.y -= 1
-	if Input.is_action_pressed("down"):
-		motion.y += 1
-	if Input.is_action_pressed("left"):
-		get_node( "Sprite" ).set_flip_h( true )
-		motion.x -= 1
-	if Input.is_action_pressed("right"):
-		get_node( "Sprite" ).set_flip_h( false )
-		motion.x += 1 
-	
-	motion = motion.normalized()
-	motion = move_and_slide(motion * movespeed)
-	
-	if Input.is_action_pressed("LMB") and Global.criacao_no_pai != null and recarregado:
+	if Input.is_action_pressed("LMB") and Global.criacao_no_pai != null and recarregado and !morto:
 		Global.instance_node(bullet, global_position, Global.criacao_no_pai)
 		recarregado = false
 		$tempo_recarga.start()
@@ -41,6 +42,9 @@ func _on_Area2D_area_entered(area):
 		death()
 
 func death():
+	visible = false
+	morto = true
+	yield(get_tree().create_timer(1),"timeout")
 	get_tree().reload_current_scene()
 
 
